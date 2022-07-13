5539d336114a1ca702b72760f71a2fecfb648ca0
cuongKinChu <cuong.tranhahuu@ncc.asia>
2022-07-13 16:32:35 +0700

    Four Commit

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,7 +15,7 @@ class ProductSeeder extends Seeder
     public function run()
     {
         DB::table('products')->insert([
-            [ 'product_name' => 'Milk tea star', 'image' => 'milk-tea-start.jpg','price' => 29000, 'description' => 'Trà sữa trân châu hoàng kim siêu ngon','type_id'],
+            [ 'product_name' => 'Milk tea star', 'image' => 'milk-tea-start.jpg','price' => 29000, 'description' => 'Trà sữa trân châu hoàng kim siêu ngon'],
             [ 'product_name' => 'Milk tea brother', 'image' => 'milk-tea-brother.jpg','price' => 29000, 'description' => 'Trà sữa ba anh em'],
             [ 'product_name' => 'Grass Jelly Milk Coffee', 'image' => 'Grass-Jelly-Milk-Coffee.png','price' => 29000, 'description' => 'Chưa có thông tin'],
             [ 'product_name' => 'Tiger Sugar', 'image' => 'tiger-sugar.jpg','price' => 29000, 'description' => 'Trà sữa đường hổ'],
