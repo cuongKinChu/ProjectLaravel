d2642ba8edf4cb05115114e2ef3e2ac318e8e63b
cuongKinChu <cuong.tranhahuu@ncc.asia>
2022-07-13 20:24:32 +0700

    Second Commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Admin
+Route::group([
+    'namespace' => 'Admin',
+    'prefix' => 'admin',
+    'name' => 'admin.',
+],
+    function () {
+        Route::get('/login', [LoginController::class, 'index'])->name('login');
+    });
