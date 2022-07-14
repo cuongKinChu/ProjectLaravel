acca37e3d2ee6e2ff1aa5484cdecaddee4fd35f1
cuongKinChu <cuong.tranhahuu@ncc.asia>
2022-07-14 09:04:40 +0700

    Show product in home page

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -99,75 +99,37 @@
 
             <div class="filters-content">
                 <div class="row grid">
+                    @foreach($data as $product)
                     <div class="col-sm-6 col-lg-4 all pizza">
                         <div class="box">
                             <div>
                                 <div class="img-box">
-                                    <img src="images/f1.png" alt="">
+                                    <img src="{{ asset('product-img') }}/{{ $product->image }}" alt="">
                                 </div>
                                 <div class="detail-box">
                                     <h5>
-                                        Delicious Pizza
+                                        {{ $product->product_name }}
                                     </h5>
                                     <p>
-                                        Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam
-                                        voluptatem repellendus sed eaque
+                                        {{ $product->description }}
                                     </p>
                                     <div class="options">
                                         <h6>
-                                            $20
+                                           {{ number_format($product->price,0,',','.') }} VND
                                         </h6>
                                         <a href="">
-
+                                            <i class="fa-solid fa-cart-shopping text-white"></i>
                                         </a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
+                    @endforeach
                 </div>
             </div>
         </div>
     </section>
 
     <!-- end food section -->
-
-    <!-- about section -->
-
-    <section class="about_section layout_padding">
-        <div class="container  ">
-
-            <div class="row">
-                <div class="col-md-6 ">
-                    <div class="img-box">
-                        <img src="images/about-img.png" alt="">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="detail-box">
-                        <div class="heading_container">
-                            <h2>
-                                We Are Feane
-                            </h2>
-                        </div>
-                        <p>
-                            There are many variations of passages of Lorem Ipsum available, but the majority have
-                            suffered alteration
-                            in some form, by injected humour, or randomised words which don't look even slightly
-                            believable. If you
-                            are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything
-                            embarrassing hidden in
-                            the middle of text. All
-                        </p>
-                        <a href="">
-                            Read More
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <!-- end about section -->
 @endsection
